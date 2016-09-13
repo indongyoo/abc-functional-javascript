36741c66529fd3255c5c28ce6e7d6865f8867480
Hanah Choi <choi.hanah92@gmail.com>
2016-09-13 12:03:44 +0900

    Update abc.js
    H => start_tag 함수 : attrs  수정

--- example/js/abc.js
+++ example/js/abc.js
@@ -431,14 +431,14 @@
         // attrs
         str = str.replace(/\[(.*)\]/, function(match, inner) { return (attrs += ' ' + inner) && ''; });
 
-        // attrs += id
-        attrs += [''].concat(C.map(str.match(/#(\{\{\{.*?\}\}\}|\{\{.*?\}\}|[\w\-]+)/g), function(v) { return v.slice(1); })).join(' id=');
-
         // attrs += class
         (cls = C.map(str.match(/\.(\{\{\{.*?\}\}\}|\{\{.*?\}\}|[\w\-]+)/g), function(v) { return v.slice(1); }).join(' '))
         && attrs == (attrs = attrs.replace(/class\s*=\s*((\").*?\"|(\{.*?\}|\S)+)/,
             function(match, tmp, q) { return ' class=' + '"' + cls + ' ' + (q ? tmp.slice(1,-1) : tmp)  + '"'; }))
-        && (attrs += ' class="' + cls + '"');
+        && (attrs = ' class="' + cls + '"' + attrs);
+
+        // attrs += id
+        attrs = [''].concat(C.map(str.match(/#(\{\{\{.*?\}\}\}|\{\{.*?\}\}|[\w\-]+)/g), function(v) { return v.slice(1); })).join(' id=') + attrs;
 
         return '<' + name + attrs + ' >'; // 띄어쓰기 <a href=www.marpple.com/> 를 위해
     }
