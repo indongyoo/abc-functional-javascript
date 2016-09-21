dce64f564b6ccb61b56dc7a613de217303425a31
hanahdool <choi.hanah92@gmail.com>
2016-09-21 11:34:33 +0900

    07. each...map...find....html

--- example/js/abc.js
+++ example/js/abc.js
@@ -493,8 +493,7 @@
     function I(v) { return v; }
 
     function IF(predicate, fn) {
-        //if (!_.isFunction(predicate))
-        //    predicate = (function(predicate) { return function(val) { return _.isEqual(val, predicate); } })(predicate);
+        //if (!_.isFunction(predicate)) predicate = (function(predicate) { return function(val) { return _.isEqual(val, predicate); } })(predicate);
         var store = [fn ? [predicate, fn] : [I, predicate]];
 
         return _.extend(IF, {
