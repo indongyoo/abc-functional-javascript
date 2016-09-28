f427bc7843eaf551db2a285b2dcd821dc1859287
park10df@piljung.kr <park10df@piljung.kr>
2016-09-28 13:00:01 +0900

    C.lambda

--- etc/abc.all.js
+++ etc/abc.all.js
@@ -873,13 +873,6 @@ function respect_underscore(_) {
 !function (root, cLambda, makeConstructorBox) {
   root.Box = makeConstructorBox(root, cLambda);
 }(typeof global == 'object' && global.global == global && (global.G = global) || window, function (C) {
-  var __slice = Array.prototype.slice;
-
-  // ## Functionalizing
-  //
-  // The utility functions operate on other functions. They can also operate on string
-  // abbreviations for functions by calling `functionalize(...)` on their inputs.
-
   // SHIM
   if ('ab'.split(/a*/).length < 2) {
     if (typeof console !== "undefined" && console !== null) {
@@ -888,6 +881,7 @@ function respect_underscore(_) {
   }
 
   function to_function(str) {
+    if (typeof str !== 'string') return str;
     var expr, leftSection, params, rightSection, sections, v, vars, _i, _len;
     params = [];
     expr = str;
@@ -926,25 +920,7 @@ function respect_underscore(_) {
     return is_ ? f : f();
   }
 
-  function functionalize(fn) {
-    if (typeof fn === 'function') {
-      return fn;
-    } else if (typeof fn === 'string' && /^[_a-zA-Z]\w*$/.test(fn)) {
-      return function () {
-        var args, receiver, _ref;
-        receiver = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-        return (_ref = receiver[fn]).call.apply(_ref, [receiver].concat(__slice.call(args)));
-      };
-    } else if (typeof fn.lambda === 'function') {
-      return fn.lambda();
-    } else if (typeof fn.toFunction === 'function') {
-      return fn.toFunction();
-    } else if (typeof fn === 'string') {
-      return to_function(fn);
-    }
-  }
-
-  return C.lambda = functionalize;
+  return C.lambda = to_function;
 }(C), function makeBox(root, cLambda) {
   var Box = function Box(key, value) {
     var cache = {};
