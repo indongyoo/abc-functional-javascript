9bf13afa24c83350b4489a44cb5d9fb8026592ca
park10df@piljung.kr <park10df@piljung.kr>
2016-09-30 16:04:04 +0900

    change createBox

--- etc/abc.all.js
+++ etc/abc.all.js
@@ -975,80 +975,68 @@ function respect_underscore(_) {
 }
 //-------------------- abc.box.js -----------------------
 !function (root, makeConstructorBox) {
-  root.Box = makeConstructorBox(root);
+  root.createBox = root.create_box = makeConstructorBox(root);
 }(typeof global == 'object' && global.global == global && (global.G = global) || window, function makeBox(root) {
-  var Box = function Box(key, value) {
-    var data = {}, cache = {};
-    var is_string = root.C.isString(key), k;
-    if (is_string && arguments.length == 2) data[key] = value;
-    else if (!is_string && arguments.length == 1) for (k in key) data[k] = key[k];
-    this._ = function () { return data; }; // 괜찮은 이름 추천해주세요~
-    this.__cache__ = function () { return cache; };
-  };
-
   function make_selector(el) {
     return root.C.isString(el) ? el : (root.C.isArrayLike(el) ? el[0] : el).getAttribute('box_selector');
   }
-
-  Box.prototype.select = Box.prototype.sel = function (el, is_init_cache) {
-    if (!el || root.C.isArrayLike(el) && !el.length) return ;
-    var selector = make_selector(el);
-    var _data = root.C.select(this._(), selector);
-    var cache = this.__cache__(), _cache_val = cache[selector];
-    return (is_init_cache || !_cache_val) ? (cache[selector] = _data) : _cache_val;
+  return function (key, value) {
+    var _box_data = {}, _box_cache = {};
+    var is_string = root.C.isString(key), k;
+    if (is_string && arguments.length == 2) _box_data[key] = value;
+    else if (!is_string && arguments.length == 1) for (k in key) _box_data[k] = key[k];
+    return {
+      _: function () { return _box_data; },
+      select: function (el, is_init_cache) {
+        if (!el || root.C.isArrayLike(el) && !el.length) return ;
+        var selector = make_selector(el);
+        var _data = root.C.select(_box_data, selector);
+        var _cache_val = _box_cache[selector];
+        return (is_init_cache || !_cache_val) ? (_box_cache[selector] = _data) : _cache_val;
+      },
+      set: function (el, value) {
+        if (arguments.length == 1 &&  root.C.isObject(el)) return root.C.extend(_box_data, el);
+        var selector = make_selector(el);
+        var result = root.C.sel.set(_box_data, selector, value);
+        _box_cache[selector] = result[0];
+        return result;
+      },
+      unset: function(el) {
+        var selector = make_selector(el);
+        var result = root.C.sel.unset(_box_data, selector);
+        _box_cache[selector] = result[0];
+        return result;
+      },
+      remove: function(el) {
+        var selector = make_selector(el);
+        var result  = root.C.sel.remove(_box_data, selector);
+        _box_cache[selector] = result[0];
+        return result;
+      },
+      extend: function(el) {
+        var selector = make_selector(el);
+        return _box_cache[selector] = root.C.sel.extend.apply(null, [_box_data, selector].concat(root.C.toArray(arguments).slice(1, arguments.length)));
+      },
+      defaults: function(el) {
+        var selector = make_selector(el);
+        return _box_cache[selector] = root.C.sel.defaults.apply(null, [_box_data, selector].concat(root.C.toArray(arguments).slice(1, arguments.length)));
+      },
+      pop: function(el) {
+        var selector = make_selector(el);
+        return root.C.sel.pop(_box_data, selector);
+      },
+      push: function(el, item) {
+        var selector = make_selector(el);
+        return root.C.sel.push(_box_data, selector, item);
+      },
+      shift: function(el) {
+        var selector = make_selector(el);
+        return root.C.sel.shift(_box_data, selector);
+      },
+      unshift: function(el, item) {
+        var selector = make_selector(el);
+        return root.C.sel.unshift(_box_data, selector, item);
+      }
+    };
   };
-
-  Box.prototype.set = function (el, value) {
-    if (arguments.length == 1 &&  root.C.isObject(el)) return root.C.extend(this._(), el) && this;
-    var selector = make_selector(el);
-    var result = root.C.sel.set(this._(), selector, value);
-    this.__cache__()[selector] = result[0];
-    return result;
-  };
-
-  Box.prototype.unset = function(el) {
-    var selector = make_selector(el);
-    var result = root.C.sel.unset(this._(), selector);
-    this.__cache__()[selector] = result[0];
-    return result;
-  };
-
-  Box.prototype.remove = function(el) {
-    var selector = make_selector(el);
-    var result  = root.C.sel.remove(this._(), selector);
-    this.__cache__()[selector] = result[0];
-    return result;
-  };
-
-  Box.prototype.extend = function(el) {
-    var selector = make_selector(el);
-    return this.__cache__()[selector] = root.C.sel.extend.apply(null, [this._(), selector].concat(root.C.toArray(arguments).slice(1, arguments.length)));
-  };
-
-  Box.prototype.defaults = function(el) {
-    var selector = make_selector(el);
-    return this.__cache__()[selector] = root.C.sel.defaults.apply(null, [this._(), selector].concat(root.C.toArray(arguments).slice(1, arguments.length)));
-  };
-
-  Box.prototype.pop = function(el) {
-    var selector = make_selector(el);
-    return root.C.sel.pop(this._(), selector);
-  };
-
-  Box.prototype.push = function(el, item) {
-    var selector = make_selector(el);
-    return root.C.sel.push(this._(), selector, item);
-  };
-
-  Box.prototype.shift = function(el) {
-    var selector = make_selector(el);
-    return root.C.sel.shift(this._(), selector);
-  };
-
-  Box.prototype.unshift = function(el, item) {
-    var selector = make_selector(el);
-    return root.C.sel.unshift(this._(), selector, item);
-  };
-
-  return Box;
 });
